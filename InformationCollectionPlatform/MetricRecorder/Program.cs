a31f58a6ae7588dd953631c3379938b1c03290d9
ShengChang <changsheng@kooboo.cn>
2018-12-20 16:15:10 +0800

    unload corrupted project and add a test project.

---
+++ InformationCollectionPlatform/MetricRecorder/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MetricRecorder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("---------------Metric Recorder: logistic information---------------");
+            //Console.ReadKey();
+        }
+    }
+}
